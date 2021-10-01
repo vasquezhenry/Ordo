53ec5b952156a558ae691fcc8ccd76bf4e8a8eaa
Henry Vasquez <henry.vasquez@nrg-edge.com>
2021-10-01 11:26:52 -0400

    add api interface

--- client/src/app/api/index.ts
+++ client/src/app/api/index.ts
@@ -1,4 +1,13 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+import {
+  Category,
+  CreateCategoryDto,
+  CreateItemDto,
+  CreateRestaurantDto,
+  Item,
+  Restaurant,
+  UpdateItemDto,
+} from '../types';
 
 //Constant global key name used to get access token from local storage
 export const JWT_KEY = 'access_token';
@@ -22,3 +31,44 @@ instance.interceptors.request.use(
     return Promise.reject(err);
   }
 );
+const Restaurants = {
+  //Create a restaurant for this ownerId
+  async createRestaurant(ownerId: string, r: CreateRestaurantDto): Promise<AxiosResponse<Restaurant>> {
+    return instance.post(`/owners/${ownerId}/restaurants`, r);
+  },
+  //All the restaurants given a ownerId
+  async getRestaurants(ownerId: string): Promise<AxiosResponse<Restaurant>> {
+    return instance.get(`/owners/${ownerId}/restaurants`);
+  },
+};
+const Items = {
+  //Create item given a categoryId
+  async createItem(categoryId: string, item: CreateItemDto): Promise<AxiosResponse<Item>> {
+    return instance.post(`/categories/${categoryId}/items`, item);
+  },
+  //Get all items by categoryId
+  async getItems(categoryId: string): Promise<AxiosResponse<Item>> {
+    return instance.get(`/categories/${categoryId}/items`);
+  },
+  //Update an item by itemId(NOTE: If you want to "delete" an item just change active to false on the item)
+  async updateItem(itemId: string, item: UpdateItemDto): Promise<AxiosResponse<Item>> {
+    return instance.put(`/items/${itemId}`, item);
+  },
+};
+
+const Categories = {
+  //Create category given menuId and category dto
+  async createCategory(menuId: string, category: CreateCategoryDto): Promise<AxiosResponse<Category>> {
+    return instance.post(`/menus/${menuId}/categories`, category);
+  },
+  //Get all categories by a given menuId
+  async getCategories(menuId: string): Promise<AxiosResponse<Category>> {
+    return instance.get(`/menus/${menuId}/categories`);
+  },
+};
+
+export const API = {
+  Restaurants,
+  Items,
+  Categories,
+};
---
+++ client/src/app/types.ts
@@ -0,0 +1,70 @@
+export interface RestaurantAddressDto {
+  address1: string;
+  address2?: string;
+  address3?: string;
+  address4?: string;
+  city: string;
+  state: string;
+  country: string;
+  postalCode: string;
+  phoneNumber: string;
+}
+export interface CreateRestaurantDto {
+  id: string;
+  name: string;
+  ownerId: string;
+  description?: string;
+  addresses: RestaurantAddressDto[];
+}
+
+export interface Restaurant {
+  id: string;
+  name: string;
+  type: string;
+  ownerId: string;
+  description: string;
+  menuId: string;
+  addresses: RestaurantAddressDto[];
+}
+
+export interface Menu {
+  id: string;
+  description: string;
+  restaurantId: string;
+  categories: Category[];
+}
+
+export interface Category {
+  id: string;
+  name: string;
+  description: string;
+  menuId: string;
+}
+
+export interface CreateCategoryDto {
+  name: string;
+  description: string;
+}
+
+export interface Item {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  active: boolean;
+  categoryId: string;
+}
+
+export interface CreateItemDto {
+  name: string;
+  description: string;
+  price: number;
+}
+
+export interface UpdateItemDto {
+  name: string;
+  description?: string;
+  price: number;
+  active: boolean;
+  categoryId: string;
+}
