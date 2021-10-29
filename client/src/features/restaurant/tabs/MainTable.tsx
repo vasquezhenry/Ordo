43599bc01138d1b6fb6dac175145d4edee43099a
IrshadYasin <irsdysn@gmail.com>
2021-10-29 16:26:53 -0400

    button styling

--- client/src/features/restaurant/tabs/MainTable.tsx
+++ client/src/features/restaurant/tabs/MainTable.tsx
@@ -1,4 +1,4 @@
-import { Button, Dialog, DialogContent, DialogTitle, Paper, Typography } from "@mui/material";
+import { Box, Button, Dialog, DialogContent, DialogTitle, Paper, Typography } from "@mui/material";
 import React from "react";
 import { API } from "../../../app/api";
 import { Category, CreateCategoryDto } from "../../../app/types";
@@ -58,16 +58,18 @@ export default function MenuTable({menuId}: MenuTableProps) {
   }
   return (
     <> 
-      <Button onClick={handleClickOpen}>
+      <Button onClick={handleClickOpen} variant= "contained" style= {{float: "right" , marginBottom: "10px"}}>
         Add Category
+
       </Button>
+      <Box sx={{ width: '75%', typography: 'body1' }}>
       {categories.map((category) => {
         return (
           <Paper elevation={3} >
-            <Button onClick={() => handleDelete(category.id)}>
+            <Button onClick={() => handleDelete(category.id)} style = {{float: "right"}}>
               Delete
             </Button>
-            <Button>Add Item</Button>
+            <Button style = {{float: "right"}} variant = "contained">Add Item</Button>
             <Typography variant="h4" component="div">
               {category.name}
             </Typography>
@@ -75,6 +77,7 @@ export default function MenuTable({menuId}: MenuTableProps) {
           </Paper>
         )
       })}
+      </Box>
       <Dialog
       open={open}
       onClose={handleClose}
