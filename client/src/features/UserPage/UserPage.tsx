63899510262c4b16432a50ed6b715cbe4309bc86
alcl00 <aclevinson@gmail.com>
2021-11-12 08:24:52 -0500

    Update UserPage.tsx

--- client/src/features/UserPage/UserPage.tsx
+++ client/src/features/UserPage/UserPage.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEvent, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
 import { Button, Box, Card, TextField } from "@mui/material"
 
